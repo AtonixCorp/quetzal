45445fe16db2a0cb3a75af1f891455100a707186
guxegdsa <ofidohub@gmail.com>
2024-12-10 05:37:26 +0000

    ab7b08d Initial commit

--- Terraform/agents/main.tf
+++ Terraform/agents/main.tf
@@ -1,20 +1,10 @@
-terraform {
-  required_providers {
-    kubernetes = {
-      source = "hashicorp/kubernetes"
-      version = "2.33.0"
-    }
-  }
+terraform { 
+  cloud { 
+    
+    organization = "AtonixCorp" 
 
-  cloud {
-    organization = "AtonixCorp"
-
-    workspaces {
-      name = "AtonixCorp"
-    }
-  }
-}
-
-provider "kubernetes" {
-  config_path = "~/.kube/config"
+    workspaces { 
+      name = "AtonixCore" 
+    } 
+  } 
 }
