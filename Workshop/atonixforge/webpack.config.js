97fae0ceef5201a0f8de8fc3651ebc7ed3678f49
guxegdsa <ofidohub@gmail.com>
2025-01-01 18:29:24 -0500

    developed and fixed

--- Workshop/atonixforge/webpack.config.js
+++ Workshop/atonixforge/webpack.config.js
@@ -1,74 +1,26 @@
 const path = require('path');
-const autoprefixer = require('autoprefixer');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  mode: 'development',
-  entry: './src/App.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public'),
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    compress: true,
-    port: 9000,
-    hot: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({ template: './src/public/index.html' }),
-    new MiniCssExtractPlugin(),
-  ],
+  // ... other configurations ...
   module: {
     rules: [
       {
-        test: /\.(scss)$/,
+        test: /\.scss$/,
         use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-          },
+          'style-loader',
           'css-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              postcssOptions: {
-                plugins: [autoprefixer()],
-              },
-            },
-          },
           {
             loader: 'sass-loader',
             options: {
+              implementation: require('sass'),
               sassOptions: {
-                quietDeps: true,
+                silenceDeprecations: ['legacy-js-api'],
               },
             },
           },
         ],
       },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-          },
-          'css-loader',
-        ],
-      },
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
     ],
   },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  stats: {
-    children: true,
-  },
+  // ... other configurations ...
 };
